80fc60234db7468e7442d3911eeae4e40f1a679b
alandioda <alandioda>
2018-03-21 18:49:15 +0100

    Added weapons code (tests)

---
+++ Miptoon/src/tests/GameWeapons.java
@@ -0,0 +1,5 @@
+package me.alandioda;
+
+public class GameWeapons {
+
+}
---
+++ Miptoon/src/tests/SplMenu.java
@@ -0,0 +1,15 @@
+package me.alandioda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.entity.ArmorStand;
+
+public class SplMenu {
+	Location playerLocation;
+	List<ArmorStand> holders = new ArrayList<ArmorStand>();
+	GameMode gamemode;
+	String menuName;
+}
