9492e1105163bf50f70dbb4729eaa9e3a063f51a
alandioda <alandioda@gmail.com>
2018-03-21 16:51:48 +0100

    the start of splatoon type of game!

---
+++ Miptoon/src/me/alandioda/PlayerData.java
@@ -0,0 +1,11 @@
+package me.alandioda;
+
+import org.bukkit.entity.Player;
+
+public class PlayerData {
+	Player player;
+	int teamColor;
+	boolean isAlive;
+	boolean kills;
+	boolean deaths;
+}
---
+++ Miptoon/src/me/alandioda/SplGame.java
@@ -0,0 +1,41 @@
+package me.alandioda;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+public class SplGame {
+	
+	//play mode variables
+	Map<Integer, Integer> schID = new HashMap<Integer, Integer>();
+	Map<String, PlayerData> players = new HashMap<String, PlayerData>();
+	List<Block> changedBlocks = new ArrayList<Block>();
+	Miptoon.GameStage gameStage = Miptoon.GameStage.DISABED;
+	int gameTime;
+	int teamColor1;
+	int teamColor2;
+	
+	
+	//game display data
+	Scoreboard gameBoard;
+	Objective obj;
+	Score website; // mipcraft.eu
+	Team timeDispl; // Time: 7:00
+	Team playersDispl; // ❤❤❤❤ ❤❤❤❤
+	
+	//game save data
+	String gamePath;
+	Location Bound1;
+	Location Bound2;
+	Location Lobby;
+	Location Team1;
+	Location Team2;
+}
